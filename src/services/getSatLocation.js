54e1fcb9452c25ea7ba43050a1019771740acece
Aashray Katiyar <aashray446@gmail.com>
2022-10-02 09:04:20 +0545

    <feature> : [added support for tle]

--- src/services/getSatLocation.js
+++ src/services/getSatLocation.js
@@ -1,13 +1,32 @@
 const axios = require('axios');
+const { getSatelliteInfo } = require("tle.js");
+const moment = require('moment');
 
-let long, lat;
+let long, lat, height, velocity, vector;
 
-let vector;
+
+// Should be periodically fetched from api
+// https://celestrak.org/NORAD/elements/stations.txt
+const tle = `ISS (ZARYA)             
+1 25544U 98067A   22274.89635759 -.00065216  00000+0 -11607-2 0  9997
+2 25544  51.6407 167.9021 0003213 257.6590 179.5260 15.49594169361737`;
+
+
+// this should be taken from user to get the location of the satellite on desired date and time
+let ts = moment("10/15/2014 9:00:59", "M/D/YYYY H:mm:ss").valueOf();
+
+
 
 const getPositionOfISS = async () => {
-    const response = await axios.get('https://api.wheretheiss.at/v1/satellites/25544');
-    long = response.data.longitude;
-    lat = response.data.latitude;
+    // get the position of the satellite at current time
+    const optionalTimeStamp = moment().valueOf();
+    const stateLiteInfo = getSatelliteInfo(tle, optionalTimeStamp);
+
+    long = stateLiteInfo.lng;
+    lat = stateLiteInfo.lat;
+    height = stateLiteInfo.height / 1000;
+    velocity = stateLiteInfo.velocity;
+
 }
 
 
@@ -15,7 +34,7 @@ const getPositionOfISS = async () => {
 const updateLatLong = () => {
     return new Promise((resolve, reject) => {
         getPositionOfISS()
-        resolve(convertLatLangToCartersian(lat, long, 0.4))
+        resolve(convertLatLangToCartersian(lat, long, height))
     })
 }
 
