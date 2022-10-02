a21c8957f26d4a2adebabe55b8c18acd560df466
Aashray Katiyar <aashray446@gmail.com>
2022-10-02 22:55:50 +0545

    <build> : []

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -8,81 +8,81 @@ module.exports = {
     output:
     {
         filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../docs')
     },
     devtool: 'source-map',
     plugins:
-    [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
-        new MiniCSSExtractPlugin()
-    ],
+        [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static') }
+                ]
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../src/index.html'),
+                minify: true
+            }),
+            new MiniCSSExtractPlugin()
+        ],
     module:
     {
         rules:
-        [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
+            [
+                // HTML
+                {
+                    test: /\.(html)$/,
+                    use: ['html-loader']
+                },
 
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
-                ]
-            },
+                // JS
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use:
+                        [
+                            'babel-loader'
+                        ]
+                },
 
-            // CSS
-            {
-                test: /\.css$/,
-                use:
-                [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
+                // CSS
+                {
+                    test: /\.css$/,
+                    use:
+                        [
+                            MiniCSSExtractPlugin.loader,
+                            'css-loader'
+                        ]
+                },
 
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/images/'
-                        }
-                    }
-                ]
-            },
+                // Images
+                {
+                    test: /\.(jpg|png|gif|svg)$/,
+                    use:
+                        [
+                            {
+                                loader: 'file-loader',
+                                options:
+                                {
+                                    outputPath: 'assets/images/'
+                                }
+                            }
+                        ]
+                },
 
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/fonts/'
-                        }
-                    }
-                ]
-            }
-        ]
+                // Fonts
+                {
+                    test: /\.(ttf|eot|woff|woff2)$/,
+                    use:
+                        [
+                            {
+                                loader: 'file-loader',
+                                options:
+                                {
+                                    outputPath: 'assets/fonts/'
+                                }
+                            }
+                        ]
+                }
+            ]
     }
 }
